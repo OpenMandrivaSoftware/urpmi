OpenMandrivaSoftware/urpmi

commit 374930bf6419bfa2e994fe3dc8eca2a89ae6127e

there is no reason search_packages should return multiple pkgs with same fullname when using {provides}, and should not otherwise
(will cleanup duplication in next commit)


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/urpmi/trunk@231535 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

urpm/select.pm
@@ -179,9 +179,12 @@ sub search_packages {
 			    $best = $_;
 			}
 		    }
-		    $packages->{$best->id} = 1;
-		    $urpm->{debug} and $urpm->{debug}("search_packages: found " . $best->fullname . " matching $v");
-		    $best->set_flag_skip(0); #- reset skip flag as manually selected.
+		    my @l = grep { $_->fullname eq $best->fullname } @$_;
+		    $packages->{join('|', map { $_->id } @l)} = 1;
+		    foreach my $pkg (@l) {
+			$urpm->{debug} and $urpm->{debug}("search_packages: found " . $pkg->fullname . " matching $v");
+			$pkg->set_flag_skip(0); #- reset skip flag as manually selected.
+		    }
 		}
 	    }
 	}

commit 7745035586b1934fdd381220906b8a76233e4689

ensure "urpmq --fuzzy" displays each packages alone on its line.
this regression was introduced in commit r231535:

	  there is no reason search_packages should return multiple
	  pkgs with same fullname when using {provides}, and should
	  not otherwise

a "better" fix could be done in urpmq.

nb: "urpmq -d basesystem" still use "|" for choices, but 
    "urpmq --sources -d basesystem" doesn't, so it's quite broken...


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/urpmi/trunk@231782 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

urpm/select.pm
@@ -80,8 +80,12 @@ sub search_packages {
     my ($name2ids, $result) = _search_packages($urpm, $names, %options) or return;
 
     foreach my $v (@$names) {
-	$packages->{$name2ids->{$v}} = 1;
-	foreach (split /\|/, $name2ids->{$v}) {
+	my @ids = split /\|/, $name2ids->{$v};
+
+	#- in case we have a substring match, we want individual selection (for urpmq --fuzzy)
+	$packages->{$_} = 1 foreach $result eq 'substring' ? @ids : $name2ids->{$v};
+
+	foreach (@ids) {
 	    my $pkg = $urpm->{depslist}[$_] or next;
 	    $urpm->{debug} and $urpm->{debug}("search_packages: found " . $pkg->fullname . " matching $v");
 	    $pkg->set_flag_skip(0); #- reset skip flag as manually selected.