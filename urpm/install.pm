OpenMandrivaSoftware/urpmi

commit 785754ca76962f0776a12df817d0f3d655642e49

(install) add more callbacks for installer
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/urpmi/trunk@253510 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

urpm/install.pm
@@ -227,12 +227,14 @@ sub install {
 	$options{callback_open} = sub {
 	    my ($_data, $_type, $id) = @_;
 	    $index++;
+	    $options{callback_open_helper} and $options{callback_open_helper}->(@_);
 	    $fh = urpm::sys::open_safe($urpm, '<', $install->{$id} || $upgrade->{$id});
 	    $fh ? fileno $fh : undef;
 	};
 	$options{callback_close} = sub {
 	    my ($urpm, undef, $pkgid) = @_;
 	    return unless defined $pkgid;
+	    $options{callback_close_helper} and $options{callback_close_helper}->(@_);
 	    get_README_files($urpm, $trans, $urpm->{depslist}[$pkgid]);
 	    close $fh if defined $fh;
 	};

urpm/main_loop.pm
@@ -220,10 +220,13 @@ foreach my $set (@{$state->{transaction} || []}) {
 		urpm::install::options($urpm),
 		test => $test,
 		verbose => $options{verbose},
+		script_fd => $urpm->{options}{script_fd},
 		oldpackage => $state->{oldpackage},
 		justdb => $options{justdb},
 		replacepkgs => $options{replacepkgs},
+		callback_close_helper => $callbacks->{close_helper},
 		callback_inst => $callbacks->{inst},
+		callback_open_helper => $callbacks->{open_helper},
 		callback_trans => $callbacks->{trans},
 		callback_report_uninst => $callbacks->{callback_report_uninst},
 		raw_message => 1,

commit 436e19d51a95d33979ba58475427cb1b7eb6ce49

(install) fix a circular reference that was causing rpmdb to be opened many times in installer
(regression introduced in r253510 when adding new callbacks for the installer)


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/urpmi/trunk@254652 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

urpm/install.pm
@@ -224,17 +224,21 @@ sub install {
 	my $fh;
 	#- assume default value for some parameter.
 	$options{delta} ||= 1000;
+
+	#- ensure perl does not create a circular reference below, otherwise all this won't be collected,
+	#  and rpmdb won't be closed:
+	my ($callback_open_helper, $callback_close_helper) = ($options{callback_open_helper}, $options{callback_close_helper});
 	$options{callback_open} = sub {
 	    my ($_data, $_type, $id) = @_;
 	    $index++;
-	    $options{callback_open_helper} and $options{callback_open_helper}->(@_);
+	    $callback_open_helper and $callback_open_helper->(@_);
 	    $fh = urpm::sys::open_safe($urpm, '<', $install->{$id} || $upgrade->{$id});
 	    $fh ? fileno $fh : undef;
 	};
 	$options{callback_close} = sub {
 	    my ($urpm, undef, $pkgid) = @_;
 	    return unless defined $pkgid;
-	    $options{callback_close_helper} and $options{callback_close_helper}->($db, @_);
+	    $callback_close_helper and $callback_close_helper->($db, @_);
 	    get_README_files($urpm, $trans, $urpm->{depslist}[$pkgid]);
 	    close $fh if defined $fh;
 	};