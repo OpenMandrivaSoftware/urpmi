87ca0e19b9a905f5127c942bc80b953e7f1c0a10
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2011-01-21 11:43:27 +0000

    fix db conversion of chroot being run error return value
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/urpmi/trunk@271751 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- urpm/sys.pm
+++ urpm/sys.pm
@@ -241,7 +241,7 @@ sub migrate_rpmdb_db_version {
 
     $urpm->{info}("migrating rpm db...");
     my $convert = URPM::DB::convert($root, $dbtype, $endianness, $rebuild);
-    if(!$convert) {
+    if($convert) {
 	if(system("chroot $root sh -c 'rpm --rebuilddb -v && rpm -qa > /dev/null && rpm -q rpm > /dev/null'")) {
 	    $urpm->{error}("rpm db migration failed in $root. You will not be able to run rpm chrooted");
 	}
