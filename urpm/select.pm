771b0a586d1dc7dd98b69cb01ace5c182cf592c8
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-01-06 19:44:00 +0000

    (conflicting_packages_msg_) kill unused args
    git-svn-id: svn://svn.mageia.org/soft/rpm/urpmi/trunk@2605 5b7c3c08-40e5-403b-9995-ace06908e4af

--- urpm/select.pm
+++ urpm/select.pm
@@ -567,7 +567,7 @@ sub rejected_unsatisfied {
 }
 
 sub conflicting_packages_msg_ {
-    my ($_urpm, $_state, $removed_packages_msgs) = @_;
+    my ($removed_packages_msgs) = @_;
 
     my $list = join("\n", @$removed_packages_msgs) or return;
     @$removed_packages_msgs == 1 ? 
@@ -576,7 +576,7 @@ sub conflicting_packages_msg_ {
 }
 sub conflicting_packages_msg {
     my ($urpm, $state) = @_;
-    conflicting_packages_msg_($urpm, $state, [ removed_packages_msgs($urpm, $state) ]);
+    conflicting_packages_msg_([ removed_packages_msgs($urpm, $state) ]);
 }
 
 sub removed_packages_msgs {
