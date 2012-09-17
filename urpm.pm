2301b6c6441c5196606c8b421e475b8d1ad932dc
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-09-17 18:03:06 +0000

    let's have a link in doc
    git-svn-id: svn://svn.mageia.org/soft/rpm/urpmi/trunk@5918 5b7c3c08-40e5-403b-9995-ace06908e4af

--- urpm.pm
+++ urpm.pm
@@ -15,7 +15,7 @@ use urpm::md5sum;
 # perl_checker: require urpm::media
 # perl_checker: require urpm::parallel
 
-our $VERSION = '7.8';
+our $VERSION = '7.8.1';
 our @ISA = qw(URPM Exporter);
 our @EXPORT_OK = ('file_from_local_url', 'file_from_local_medium', 'is_local_medium');
 
@@ -489,7 +489,7 @@ on a Mandriva Linux distribution.
 =item urpm->new()
 
 The constructor creates a new urpm object. It's a blessed hash that
-contains fields from C<URPM>, and also the following fields:
+contains fields from L<URPM>, and also the following fields:
 
 B<source>: { id => src_rpm_file|spec_file }
 
