35db520389a9259149657c702dbad96b0b6b9641
Denis Silakov <denis.silakov@rosalab.ru>
2014-03-25 10:35:29 +0400

    Redesign smart-auto selection - move implementation to select.pm module from urpmi executable

--- urpm/select.pm
+++ urpm/select.pm
@@ -442,6 +442,89 @@ sub cooked_prefer {
     @{$urpm->{prefer_regexps}};
 }
 
+# Longest common substring (used in the get_best_choice() function below)
+# Taken from wikibooks
+# http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring
+sub lc_substr {
+    my ($str1, $str2) = @_;
+    my $l_length = 0; # length of longest common substring
+    my $len1 = length $str1;
+    my $len2 = length $str2;
+    my @char1 = (undef, split(//, $str1)); # $str1 as array of chars, indexed from 1
+    my @char2 = (undef, split(//, $str2)); # $str2 as array of chars, indexed from 1
+    my @lc_suffix; # "longest common suffix" table
+    my @substrings; # list of common substrings of length $l_length
+
+    for my $n1 ( 1 .. $len1 ) {
+        for my $n2 ( 1 .. $len2 ) {
+            if ($char1[$n1] eq $char2[$n2]) {
+                # We have found a matching character. Is this the first matching character, or a
+                # continuation of previous matching characters? If the former, then the length of
+                # the previous matching portion is undefined; set to zero.
+                $lc_suffix[$n1-1][$n2-1] ||= 0;
+                # In either case, declare the match to be one character longer than the match of
+                # characters preceding this character.
+                $lc_suffix[$n1][$n2] = $lc_suffix[$n1-1][$n2-1] + 1;
+                # If the resulting substring is longer than our previously recorded max length ...
+                if ($lc_suffix[$n1][$n2] > $l_length) {
+                    # ... we record its length as our new max length ...
+                    $l_length = $lc_suffix[$n1][$n2];
+                    # ... and clear our result list of shorter substrings.
+                    @substrings = ();
+                }
+                # If this substring is equal to our longest ...
+                if ($lc_suffix[$n1][$n2] == $l_length) {
+                    # ... add it to our list of solutions.
+                    push @substrings, substr($str1, ($n1-$l_length), $l_length);
+                }
+            }
+        }
+    }
+
+    return @substrings;
+}
+
+sub get_best_choice {
+    my ($urpm, $choices_in, @all_names) = @_;
+    my @choices = @$choices_in;
+
+    # Let's try to fit the best package
+    # First, let's choose the highest version
+    # If versions are the same, try the name with the longest substring
+    # matching requested features
+    #
+    my $i = 1;
+    my $best_idx = 1;
+    my $longest_len = 0;
+    foreach my $pkg (@choices) {
+        my @pkgname_string_chars = split("",$pkg->name);
+
+        if( URPM::rpmEVRcompare($pkg->evr, $choices[$best_idx-1]->evr) > 0 ) {
+            $best_idx = $i;
+
+            foreach my $name (@all_names) {
+                my @longest_strs = lc_substr($name, $pkg->name);
+                if( $longest_len < length($longest_strs[0]) ) {
+                    $longest_len = length($longest_strs[0]);
+                }
+            }
+        }
+        elsif( URPM::rpmEVRcompare($pkg->evr, $choices[$best_idx-1]->evr) == 0 ) {
+            foreach my $name (@all_names) {
+                my @longest_strs = lc_substr($name, $pkg->name);
+                if( $longest_len < length($longest_strs[0]) ) {
+                    $best_idx = $i;
+                    $longest_len = length($longest_strs[0]);
+                }
+            }
+        }
+
+        $i++;
+    }
+
+    return $best_idx;
+}
+
 sub get_preferred {
     my ($urpm, $choices, $cmdline_prefer) = @_;
 
